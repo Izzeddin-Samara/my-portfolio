ef59a18e3fd3a63224ce23c5dcf207c9e834225e
Izzsamara <izzidinsamara@gmail.com>
2024-12-31 16:24:02 +0200

    Add components directory with Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <h1>Izzeddin Samara</h1>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Skills</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+export default Navbar;
