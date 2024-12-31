7c43e372ff2c6b7042e7f4efe225466d779f5566
Izzsamara <izzidinsamara@gmail.com>
2024-12-31 19:10:11 +0200

    Add styling to About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
+        <div className='about'>
             <h1>About me</h1>
-            <div>
+            <div className='about-body'>
                 <img src="izzeddin-image.jpg" alt="Izzeddin-image" />
                 <p>Motivated Junior Full Stack Developer with a passion for creating efficient and user-friendly web applications. Proficient in both front-end and back-end development, with a strong command of languages such as JavaScript, Python, and SQL. Dedicated to driving efficiency and excellence through collaborative problem-solving and continuous improvement.</p>
             </div>
