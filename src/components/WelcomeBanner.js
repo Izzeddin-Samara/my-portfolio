e4653009ecf720e2e1536b21fc9b3e486384f73e
Izzsamara <izzidinsamara@gmail.com>
2024-12-31 17:28:54 +0200

    Add styling to WelcomeBanner component

--- src/components/WelcomeBanner.js
+++ src/components/WelcomeBanner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const WelcomeBanner = () => {
     return (
-        <div>
+        <div className="welcome">
             <h1>Welcome to Izzeddin's Portfolio</h1>
             <p>A Junior Full Stack Developer</p>
         </div>
