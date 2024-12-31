3ec5d5e7a229ebd8fa81178b2cbb356c81991176
Izzsamara <izzidinsamara@gmail.com>
2024-12-31 17:12:41 +0200

    Add WelcomeBanner component

---
+++ src/components/WelcomeBanner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WelcomeBanner = () => {
+    return (
+        <div>
+            <h1>Welcome to Izzeddin's Portfolio</h1>
+            <p>A Junior Full Stack Developer</p>
+        </div>
+    );
+}
+export default WelcomeBanner;
