0885edf7fc082eee364d3d813095c69f9c7db4ab
Izzsamara <izzidinsamara@gmail.com>
2024-12-31 17:15:25 +0200

    Import WelcomeBanner component into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import WelcomeBanner from './components/WelcomeBanner';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <WelcomeBanner/>
     </div>
   );
 }
