1dda75c6e9df530c43405b59ac71d0049fff356e
Izzsamara <izzidinsamara@gmail.com>
2025-01-01 21:44:46 +0200

    Import Projects component into App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import WelcomeBanner from './components/WelcomeBanner';
 import About from './components/About';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <WelcomeBanner/>
       <About/>
       <Skills/>
+      <Projects/>
     </div>
   );
 }
