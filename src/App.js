89b81b92e8ea66d193f3bd17a2ce23c14fead572
Izzsamara <izzidinsamara@gmail.com>
2025-01-02 15:46:47 +0200

    Import Contact component into App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import WelcomeBanner from './components/WelcomeBanner';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </div>
   );
 }
