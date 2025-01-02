7043aa56c92a6ab954e4e93ea5182ef9b350998d
Izzsamara <izzidinsamara@gmail.com>
2025-01-02 20:26:41 +0200

    Add styling to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ import { MdOutlineAlternateEmail } from "react-icons/md";
 
 const Footer = () => {
     return (
-        <div>
-            <ul>
-                <li><a href="https://github.com/Izzeddin-Samara" ><FaGithub/></a></li>
-                <li><a href="https://www.linkedin.com/in/izzeddin-samara"><FaLinkedin/></a></li>
-                <li><a href="mailto:izzidinsamara@gmail.com"><MdOutlineAlternateEmail/></a></li>
-                <li><a href="https://api.whatsapp.com/send/?phone=00972595484832&text&type=phone_number&app_absent=0"><FaWhatsapp/></a></li>
+        <div className="footer">
+            <ul className="social-links">
+                <li><a href="https://github.com/Izzeddin-Samara" ><FaGithub className="social-icon" /></a></li>
+                <li><a href="https://www.linkedin.com/in/izzeddin-samara"><FaLinkedin className="social-icon" /></a></li>
+                <li><a href="mailto:izzidinsamara@gmail.com"><MdOutlineAlternateEmail className="social-icon" /></a></li>
+                <li><a href="https://api.whatsapp.com/send/?phone=00972595484832&text&type=phone_number&app_absent=0"><FaWhatsapp className="social-icon" /></a></li>
             </ul>
                 <p>Izzeddin Samara, All Rights Reserved 2025  ©  </p>
 
