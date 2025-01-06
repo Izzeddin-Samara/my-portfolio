785f22323b66c99b77cedae255cc5711ab793479
Izzsamara <izzidinsamara@gmail.com>
2025-01-06 18:48:09 +0200

    Add laptop code icon to WelcomeBanner

--- src/components/WelcomeBanner.js
+++ src/components/WelcomeBanner.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { FaLaptopCode } from "react-icons/fa";
 
 const WelcomeBanner = () => {
     return (
         <div className="welcome">
             <h1>Welcome to Izzeddin's Portfolio</h1>
             <p>A Junior Full Stack Developer</p>
+            <FaLaptopCode size={100} />
         </div>
     );
 }
